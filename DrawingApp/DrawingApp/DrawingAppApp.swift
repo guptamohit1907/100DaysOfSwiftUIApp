f8e08f07586be19d09b6c6cdb479d791776710db
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-06-03 15:58:59 +0530

    MyFirst Commit

---
+++ DrawingApp/DrawingApp/DrawingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingAppApp.swift
+//  DrawingApp
+//
+//  Created by Mohit Gupta on 03/06/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
