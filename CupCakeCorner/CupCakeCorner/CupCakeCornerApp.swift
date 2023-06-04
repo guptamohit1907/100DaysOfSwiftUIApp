bdef44b03bba433d484e927ec846be7b848e2506
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-06-04 12:35:40 +0530

    CupCake Corner First Commit

---
+++ CupCakeCorner/CupCakeCorner/CupCakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakeCornerApp.swift
+//  CupCakeCorner
+//
+//  Created by Mohit Gupta on 04/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CupCakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
