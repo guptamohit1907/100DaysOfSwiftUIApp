546338c5d03abaa2168a291a07341bbdcd8f2f3b
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-06-10 18:32:59 +0530

    Creating a custom component with @Binding – Bookworm SwiftUI Tutorial 1/10

---
+++ BookWorm/BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Mohit Gupta on 05/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
