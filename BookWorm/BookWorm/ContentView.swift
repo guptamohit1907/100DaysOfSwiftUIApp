351c0a9c6bbb486f87d973d19175c75df6993eac
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-06-12 01:07:30 +0530

    Deleting from a Core Data fetch request – Bookworm SwiftUI Tutorial 9/10

--- BookWorm/BookWorm/ContentView.swift
+++ BookWorm/BookWorm/ContentView.swift
@@ -33,15 +33,18 @@ struct ContentView: View {
                                 
                                 Text(book.author ?? "Unknown Author")
                                     .foregroundColor(.secondary)
-
                             }
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks)
             }
-            Text("Count : \(books.count)")
                 .navigationTitle("Bookworm")
                 .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading){
+                        EditButton()
+                    }
+                    
                     ToolbarItem(placement: .navigationBarTrailing) {
                         Button{
                             showingAddScreen.toggle()
@@ -53,6 +56,14 @@ struct ContentView: View {
                     AddBookView()
                 }
         }
+    }
+    
+    func deleteBooks(at offsets : IndexSet){
+        for offset in offsets {
+            let book = books[offset]
+            moc.delete(book)
+        }
+      //  try? moc.save()
     }
 }
 
