db6e30e2d970826f369fccd2a786140c65fb6ef6
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-06-25 03:27:01 +0530

    Creating NSManagedObject subclasses – Core Data SwiftUI Tutorial 2/7

---
+++ CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Mohit Gupta on 25/06/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
