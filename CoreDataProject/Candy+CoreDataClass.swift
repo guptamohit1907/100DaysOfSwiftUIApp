90a2e48db6707fee8c9dbffcffcdd75da26aed9f
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-06-26 02:10:21 +0530

    One-to-many relationships with @FetchRequest and SwiftUI – Core Data SwiftUI Tutorial 7/7

---
+++ CoreDataProject/Candy+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Candy+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Mohit Gupta on 26/06/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Candy)
+public class Candy: NSManagedObject {
+
+}
---
+++ CoreDataProject/Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Mohit Gupta on 26/06/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
