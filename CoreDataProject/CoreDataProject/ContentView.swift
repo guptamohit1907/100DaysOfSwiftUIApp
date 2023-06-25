a72aebff91a362817ac7ab6a0739925a0907a2b1
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-06-26 01:20:11 +0530

    Dynamically filtering @FetchRequest with SwiftUI – Core Data SwiftUI Tutorial 6/ 7

--- CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -10,34 +10,39 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: [],predicate: NSPredicate(format: "NOT name BEGINSWITH[c] %@","e")) var ships : FetchedResults<Ship>
+    @State private var lastNameFilter = "A"
     
     var body: some View {
-        VStack {
-            List(ships, id: \.self){ ship in
-                Text(ship.name ?? "Unknown Name")
+        VStack{
+            
+            FilteredList(filterKey: "lastName", filterValue: lastNameFilter) { (singer : Singer) in 
+                Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
             }
             
-            Button("Add Examples"){
-                let ship1 = Ship(context : moc)
-                ship1.name = "Enterprise"
-                ship1.universe = "Star Trek"
+            Button("Add Examples") {
+                let taylor = Singer(context: moc)
+                taylor.firstName = "Taylor"
+                taylor.lastName = "Swift"
                 
-                let ship2 = Ship(context : moc)
-                ship2.name = "Defiant"
-                ship2.universe = "Star Trek"
+                let ed = Singer(context: moc)
+                ed.firstName = "Ed"
+                ed.lastName = "Shareen"
                 
-                let ship3 = Ship(context : moc)
-                ship3.name = "Millenium Falcon"
-                ship3.universe = "Star Wars"
-                
-                let ship4 = Ship(context : moc)
-                ship4.name = "Executor"
-                ship4.universe = "Star Wars"
+                let adele = Singer(context: moc)
+                adele.firstName = "Adele"
+                adele.lastName = "Adkins"
                 
                 try? moc.save()
-                
             }
+            
+            Button("Show A"){
+                lastNameFilter = "A"
+            }
+            
+            Button("Show S"){
+                lastNameFilter = "S"
+            }
+                        
         }
     }
 }
---
+++ CoreDataProject/Singer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Mohit Gupta on 26/06/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
