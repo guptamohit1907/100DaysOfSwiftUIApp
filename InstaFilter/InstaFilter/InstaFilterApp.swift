cb67740cf850e37b838b374e7f6f4f0a462790be
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-06-30 02:12:00 +0530

    Initial Project Setup

---
+++ InstaFilter/InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Mohit Gupta on 30/06/23.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
