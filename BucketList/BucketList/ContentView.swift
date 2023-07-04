c36117cb9ac3ff995e3631bb1db8b1428805d445
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-07-04 17:00:39 +0530

    Improving our map annotations – Bucket List SwiftUI Tutorial 7/12

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -11,11 +11,22 @@ import MapKit
 struct ContentView: View {
     @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
     @State private var locations = [Location]()
-    
+     
     var body: some View {
         ZStack{
             Map(coordinateRegion: $mapRegion, annotationItems : locations){location in
-                MapMarker(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
+                MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)){
+                    VStack{
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundColor(.red)
+                            .frame(width: 44,height: 44)
+                            .background(.white)
+                            .clipShape(Circle())
+                        
+                        Text(location.name)
+                    }
+                }
             }
                 .ignoresSafeArea()
             Circle()
