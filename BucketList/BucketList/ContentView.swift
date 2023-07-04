4833fc36fe5640071501a9e5a7a47a8bbf39905e
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-07-04 16:26:52 +0530

    Adding user locations to a map – Bucket List SwiftUI Tutorial 6/12

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -5,37 +5,40 @@
 //  Created by Mohit Gupta on 04/07/23.
 //
 
-import LocalAuthentication
 import SwiftUI
-
+import MapKit
 
 struct ContentView: View {
-    @State private var isUnlocked = false
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
+    @State private var locations = [Location]()
     
-    var body: some View{
-        VStack {
-            if isUnlocked{
-                Text("Unlocked")
-            } else {
-                Text("Locked")
+    var body: some View {
+        ZStack{
+            Map(coordinateRegion: $mapRegion, annotationItems : locations){location in
+                MapMarker(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
             }
-        }.onAppear(perform: authenticate)
-    }
-    func authenticate(){
-        let context = LAContext()
-        let error : NSError?
-        
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
-            let reason = "We need to unlock your data"
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason){ success, authenticationError in
-                if success {
-                    isUnlocked = true
-                } else {
-                    // there was a problem
+                .ignoresSafeArea()
+            Circle()
+                .fill(.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            VStack{
+                Spacer()
+                HStack{
+                    Spacer()
+                    Button{
+                        let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
+                        locations.append(newLocation)
+                    }label: {
+                        Image(systemName: "plus")
+                    }.padding()
+                     .background(.black.opacity(0.75))
+                     .foregroundColor(.white)
+                     .font(.title)
+                     .clipShape(Circle())
+                     .padding(.trailing)
                 }
             }
-        } else {
-            // no biometrics
         }
     }
 }
