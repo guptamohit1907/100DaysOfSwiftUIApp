04496e73fbdaf394417a5b43e572b5d35f6af71a
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-07-07 22:10:35 +0530

    Hiding and grouping accessibility data – Accessibility SwiftUI Tutorial 2_6

--- AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
+++ AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -7,37 +7,14 @@
 
 import SwiftUI
 
-
-//ales-krivec-15949
-// galina-n-189483
-//kevin-horstmann-141705
-//nicolas-tissot-335096
-
 struct ContentView: View {
-    let pictures = [
-         "ales-krivec-15949",
-         "galina-n-189483",
-         "kevin-horstmann-141705",
-         "nicolas-tissot-335096"
-    ]
-    
-    let labels = [
-         "Tulips",
-         "Frozen tree buds",
-         "Sunflowers",
-         "Fireworks"
-    ]
-    
-    @State private var selectedPictures = Int.random(in: 0...3)
     var body: some View {
-        Image(pictures[selectedPictures])
-            .resizable()
-            .scaledToFit()
-            .onTapGesture {
-                selectedPictures = Int.random(in: 0...3)
-            }.accessibilityLabel(labels[selectedPictures])
-            .accessibilityAddTraits(.isButton)
-            .accessibilityRemoveTraits(.isImage)
+        VStack{
+            Text("Your score is")
+            Text("1000")
+                .font(.title)
+        }.accessibilityElement()
+         .accessibilityLabel("Your score is 1000")
     }
 }
 
