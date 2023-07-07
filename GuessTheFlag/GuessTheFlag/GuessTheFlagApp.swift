aa710ce6187b4b8b00c5cee233f21cc5e0338d1e
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-07-07 22:31:19 +0530

    Initial Project Setup

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Mohit Gupta on 07/07/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
