4c1571f797ad2ee256dea2e124bbc6c8f8ca60e6
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-07-08 12:31:18 +0530

    Initial Project Setup

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Mohit Gupta on 08/07/23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
