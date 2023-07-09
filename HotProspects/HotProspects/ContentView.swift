bf24be0f4319864ac118cde9349688b7db723ef3
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-07-09 14:13:37 +0530

    Building our tab bar – Hot Prospects SwiftUI Tutorial 10/18

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -5,21 +5,29 @@
 //  Created by Mohit Gupta on 08/07/23.
 
 import SwiftUI
-import SamplePackage
 
 struct ContentView: View {
-    let possibleNumbers = Array(1...60)
-    
     var body: some View {
-        Text(results)
+        TabView {
+            ProspectsView(filter: .none)
+                .tabItem {
+                    Label("Everyone", systemImage: "person.3")
+                }
+            ProspectsView(filter: .contacted)
+                .tabItem {
+                    Label("Contacted", systemImage: "checkmark.circle")
+                }
+            ProspectsView(filter: .uncontacted)
+                .tabItem {
+                    Label("Uncontacted", systemImage: "questionmark.diamond")
+                }
+            MeView()
+                .tabItem {
+                    Label("Me", systemImage: "person.crop.square")
+                }
+        }
     }
-    
-    var results : String {
-        let selected = possibleNumbers.random(7).sorted()
-        let strings = selected.map(String.init)
-        return strings.joined(separator: ", ")
-    }
-  }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ HotProspects/HotProspects/MeView.swift
@@ -0,0 +1,20 @@
+//
+//  MeView.swift
+//  HotProspects
+//
+//  Created by Mohit Gupta on 09/07/23.
+//
+
+import SwiftUI
+
+struct MeView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct MeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeView()
+    }
+}
