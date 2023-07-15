106d47bc7d573db74f2c0d63364837478b825d1f
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-07-15 15:05:02 +0530

    Designing a single card view – Flashzilla SwiftUI Tutorial 7/15

---
+++ FlashZilla/FlashZilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  FlashZilla
+//
+//  Created by Mohit Gupta on 15/07/23.
+//
+
+import Foundation
+
+struct Card {
+    let prompt : String
+    let answer : String
+    
+    static let example = Card(prompt: "Who played the 13th October in Doctor Who?", answer: "Joddie Whatekar")
+}
--- FlashZilla/FlashZilla/ContentView.swift
+++ FlashZilla/FlashZilla/ContentView.swift
@@ -7,23 +7,11 @@
 
 import SwiftUI
 
-func withOptionalAnimation<Result>(_ animation : Animation? = .default, _ body : () throws -> Result) rethrows -> Result {
-    if UIAccessibility.isReduceMotionEnabled{
-        return try body()
-    } else {
-        return try withAnimation(animation, body)
-    }
-}
 
 struct ContentView: View {
-    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
     
     var body: some View {
-        Text("Hello World")
-            .padding()
-            .background(reduceTransparency ? .black : .black.opacity(0.5))
-            .foregroundColor(.white)
-            .clipShape(Capsule())
+        CardView(card: Card.example)
     }
 }
 
