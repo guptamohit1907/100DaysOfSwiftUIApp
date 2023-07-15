a90c421494ca43dc9583109b5cfee6276ece21dd
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-07-15 11:25:47 +0530

    Initial Code Setup

---
+++ FlashZilla/FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by Mohit Gupta on 15/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
