e1513fbea4de989aa3eba2dde2f01289cf4e61a9
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-07-15 16:57:16 +0530

    Counting down with a Timer – Flashzilla SwiftUI Tutorial 11/15

--- FlashZilla/FlashZilla/ContentView.swift
+++ FlashZilla/FlashZilla/ContentView.swift
@@ -17,7 +17,11 @@ extension View {
 struct ContentView: View {
     @State private var cards = Array<Card>(repeating: Card.example, count: 10)
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
-
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @Environment(\.scenePhase) var scenePhase
+    @State private var isActive = true
     
     var body: some View {
         ZStack{
@@ -25,6 +29,14 @@ struct ContentView: View {
                 .resizable()
                 .ignoresSafeArea()
             VStack{
+                Text("Time : \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.horizontal,20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(Capsule())
+                
                 ZStack{
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: cards[index]){
@@ -33,30 +45,43 @@ struct ContentView: View {
                             }
                         }
                         .stacked(at: index, in: cards.count)
-                        }
+                    }
                 }
-                }
+            }
             if differentiateWithoutColor {
                 VStack{
                     Spacer()
-                    
                     HStack{
-                       Image(systemName: "xmark.circle")
+                        Image(systemName: "xmark.circle")
                             .padding()
                             .background(.black.opacity(0.7))
                             .clipShape(Circle())
                         Spacer()
                         Image(systemName: "checkmark.circle")
-                             .padding()
-                             .background(.black.opacity(0.7))
-                             .clipShape(Circle())
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(Circle())
                     }.foregroundColor(.white)
                         .font(.largeTitle)
                         .padding()
                 }
-             }
+            }
+        }.onReceive(timer) { time in
+            guard isActive else {
+                return
+            }
+            if timeRemaining > 0 {
+                timeRemaining -= 1
             }
         }
+        .onChange(of: scenePhase) { newPhase in
+            if newPhase == .active {
+                isActive = true
+            } else {
+                isActive = false
+            }
+        }
+    }
     
     func removeCard(at index : Int){
         cards.remove(at: index)
