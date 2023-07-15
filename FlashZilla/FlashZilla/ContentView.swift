6d53041947ecd739ba9503e2f88e4e46c7f52245
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-07-15 17:13:45 +0530

    Ending the app with allowsHitTesting() – Flashzilla SwiftUI Tutorial 12/15

--- FlashZilla/FlashZilla/ContentView.swift
+++ FlashZilla/FlashZilla/ContentView.swift
@@ -15,7 +15,7 @@ extension View {
 }
 
 struct ContentView: View {
-    @State private var cards = Array<Card>(repeating: Card.example, count: 10)
+    @State private var cards = Array(repeating: Card.example, count: 10)
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     @State private var timeRemaining = 100
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
@@ -46,6 +46,14 @@ struct ContentView: View {
                         }
                         .stacked(at: index, in: cards.count)
                     }
+                }.allowsHitTesting(timeRemaining > 0)
+                
+                if cards.isEmpty{
+                    Button("Start Again",action: resetCards)
+                        .padding()
+                        .background(.white)
+                        .foregroundColor(.black)
+                        .clipShape(Capsule())
                 }
             }
             if differentiateWithoutColor {
@@ -76,7 +84,9 @@ struct ContentView: View {
         }
         .onChange(of: scenePhase) { newPhase in
             if newPhase == .active {
-                isActive = true
+                if cards.isEmpty == false{
+                    isActive = true
+                }
             } else {
                 isActive = false
             }
@@ -85,6 +95,16 @@ struct ContentView: View {
     
     func removeCard(at index : Int){
         cards.remove(at: index)
+        
+        if cards.isEmpty {
+            isActive = false
+        }
+    }
+    
+    func resetCards(){
+        cards = Array(repeating: Card.example, count: 10)
+        timeRemaining = 100
+        isActive = true
     }
 }
 
