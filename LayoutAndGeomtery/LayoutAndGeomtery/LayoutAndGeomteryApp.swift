37d59195c9e6ea06ac28525f922fdfffa7b52cbc
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-07-16 12:37:14 +0530

    Initial Code Setup

---
+++ LayoutAndGeomtery/LayoutAndGeomtery/LayoutAndGeomteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeomteryApp.swift
+//  LayoutAndGeomtery
+//
+//  Created by Mohit Gupta on 16/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeomteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
