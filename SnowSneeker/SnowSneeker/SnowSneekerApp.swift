902fe853287f07165966e7c98afe73baa3002a56
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-07-16 15:46:23 +0530

    Initial Code Setup

---
+++ SnowSneeker/SnowSneeker/SnowSneekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSneekerApp.swift
+//  SnowSneeker
+//
+//  Created by Mohit Gupta on 16/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowSneekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
